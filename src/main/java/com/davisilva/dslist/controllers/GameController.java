d9eef1e7fca1ebe377b6d04de7c04ba4c6f60c50
Davi Silva <davi.brito7226@gmail.com>
2023-05-10 15:49:16 -0300

    GET game by id

--- src/main/java/com/davisilva/dslist/controllers/GameController.java
+++ src/main/java/com/davisilva/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davisilva.dslist.dto.GameDTO;
 import com.davisilva.dslist.dto.GameMinDTO;
 import com.davisilva.dslist.entities.Game;
 import com.davisilva.dslist.services.GameService;
@@ -24,4 +26,12 @@ public class GameController {
 		List<GameMinDTO> result = gameService.finAll();
 		return result;
 	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+	
 }
--- src/main/java/com/davisilva/dslist/services/GameService.java
+++ src/main/java/com/davisilva/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.davisilva.dslist.dto.GameDTO;
 import com.davisilva.dslist.dto.GameMinDTO;
 import com.davisilva.dslist.entities.Game;
 import com.davisilva.dslist.repositories.GameRepository;
@@ -15,7 +17,12 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> finAll(){
 		
 		List<Game> result = gameRepository.findAll();
@@ -23,4 +30,6 @@ public class GameService {
 		return dto;
 		
 	}
+	
+	
 }
